74057d7c598ce59b87e1f4c7ef7bb0315240a8f9
yesimcetintas <yesimturker@outlook.com>
2022-11-17 11:09:28 +0300

    Carousel and Slick add

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -4,18 +4,26 @@ import {FaUserAlt, FaShoppingCart, FaSearch} from "react-icons/fa"
 import { useState } from 'react'
 import Search from '../ui/Search'
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
+import { useRouter } from "next/router";
 
 const Header = () => {
     const [isSearchModal, setIsSearchModal] = useState(false)
     const [isMenuModal, setIsMenuModal] = useState(false);
+
+    const router = useRouter()
+
   return (
-    <div className='h-[5.5rem] bg-secondary '>
+    // <div className='h-[5.5rem] bg-secondary '>
+    <div
+    className={`h-[5.5rem] z-50 relative ${
+      router.asPath === "/" ? "bg-transparent" : "bg-secondary"
+    }`}>
       <div className='container mx-auto flex justify-between text-white items-center h-full'>
         <Logo/>
             <nav
-                className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${
+                className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden  ${
                     isMenuModal === true && "!grid place-content-center"
-                }`}>
+                  }`}>
                 <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
                     <li className='px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer'>
                         <a href="">Home</a>
