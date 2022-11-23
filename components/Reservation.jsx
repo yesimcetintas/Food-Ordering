7c7fa69bb0db153d2511cb0527e9078137da22df
yesimcetintas <yesimturker@outlook.com>
2022-11-23 16:00:27 +0300

    Reservation input formik & yup validation

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -1,59 +1,97 @@
 import React from 'react'
 import Title from './ui/Title'
 import Input from "../components/form/input"
+import { useFormik } from 'formik';
+import { reservationSchema } from '../schema/reservation';
 
 const Reservation = () => {
+
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 4000))
+    actions.resetForm()
+  }
+
+  const {values, errors, touched, handleChange, handleSubmit, handleBlur} = useFormik({
+    initialValues:{
+      fullName:"",
+      phoneNumber:"",
+      email:"",
+      persons:"",
+      date:""
+    },
+    onSubmit,
+    validationSchema: reservationSchema,
+  })
+
     const inputs = [
         {
           id: 1,
           name: "fullName",
           type: "text",
           placeholder: "Your Full Name",
+          value: values.fullName,
+          errorMessage: errors.fullName,
+          touched: touched.fullName
         },
         {
           id: 2,
           name: "phoneNumber",
           type: "number",
           placeholder: "Your Phone Number",
+          value: values.phoneNumber,
+          errorMessage: errors.phoneNumber,
+          touched: touched.phoneNumber
         },
         {
           id: 3,
           name: "email",
           type: "email",
           placeholder: "Your Email Address",
+          value: values.email,
+          errorMessage: errors.email,
+          touched: touched.email,
         },
         {
           id: 4,
           name: "persons",
           type: "number",
           placeholder: "How Many Persons?",
+          value: values.persons,
+          errorMessage: errors.persons,
+          touched: touched.persons,
         },
         {
           id: 5,
-          name: "persons",
+          name: "date",
           type: "datetime-local",
-          placeholder: "How Many Persons?",
+          value: values.date,
+          errorMessage: errors.date,
+          touched: touched.date  
         },
       ];
   return (
     <div className='container mx-auto py-12'>
         <Title addClass="text-[40px] mb-10">Book A Table</Title>
         <div className='flex justify-between flex-wrap-reverse gap-x-10'>
-            <div className='lg:flex-1 w-full'>
+            <form className='lg:flex-1 w-full' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-y-3'>
                     {
-                        inputs.map((input)=>
-                        <Input key={input.id} {...input}/>)
+                      inputs.map((input)=>
+                      <Input 
+                        key={input.id} 
+                        {...input} 
+                        onChange={handleChange}
+                        onBlur={handleBlur}/>)
                     }
                 </div>
-                <button className="btn-primary mt-4">BOOK NOW</button>
-            </div>
+                <button className="btn-primary mt-4" type='submit'>BOOK NOW</button>
+            </form>
             <div className='lg:flex-1 w-full'>
                 <iframe 
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96748.74301317202!2d-74.25209572829749!3d40.73126228225651!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c254800f03fbe5%3A0xc3102364a9100bc7!2sThe%20Newark%20Museum%20of%20Art!5e0!3m2!1str!2str!4v1669120013423!5m2!1str!2str" 
                     allowFullScreen="" 
                     loading="lazy" 
-                    referrerpolicy="no-referrer-when-downgrade"
+                    referrerPolicy="no-referrer-when-downgrade"
                     className='h-full w-full'>
                 </iframe>
             </div>
